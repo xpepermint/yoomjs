8ec0a64b5719a1ae44ccacd32fdf7407c7c66581
xpeper <xpepermint@gmail.com>
2014-08-05 04:42:23 +0200

    application boot configuration from framework to project

--- index.js
+++ index.js
@@ -7,6 +7,11 @@
 // all the pieces (e.g. models, routes) together into a module that knows how to
 // `start` and `stop` itself.
 //
+// Every project has the `config/boot.js` file which defines how the application
+// should be configured and started. Because the load process is defined inside
+// a project, a user can modify and extend the application easily. See the
+// template file `templates/config/boot.js` to see the default boot proecss.
+//
 
 // Express application instance.
 let app = module.exports = require('koa')();
@@ -15,32 +20,18 @@ app.server = null;
 
 // Initializes the application and starts the server.
 module.exports.start = function(next) {
-  // loading settings
-  require('./lib/settings');
-  // connection mong database
-  require('./lib/connectors').connect(function() {
-    // loading models
-    require('./lib/models');
-    // loading request extensions
-    require('./lib/requests');
-    // loading controllers
-    require('./lib/controllers');
-    // starting HTTP server
-    app.server = app.listen(app.settings.httpPort, app.settings.httpAddress, next);
-    console.log("[application] Start in "+app.env+" mode on http://"+app.settings.httpAddress+":"+app.settings.httpPort);
-  });
+  // starting HTTP server
+  app.server = app.listen(app.settings.httpPort, app.settings.httpAddress, next);
+  console.log("[application] Start in "+app.env+" mode on http://"+app.settings.httpAddress+":"+app.settings.httpPort);
   // returning app
   return app;
 };
 
 // Stops the server.
 module.exports.stop = function() {
-  // stopping HTTP server
-  require('./lib/connectors').disconnect(function() {
-    app.server.close()
-    // initializing server instance variable
-    app.server = null
-  });
+  app.server.close()
+  // initializing server instance variable
+  app.server = null
   // returning app
   return app;
 };
--- templates/config/boot.js
+++ templates/config/boot.js
@@ -2,5 +2,16 @@
 
 // application instance
 let app = require('yoom');
-// lifting application
-app.start();
+// loading settings
+require('yoom/lib/settings');
+// loading request extensions
+require('yoom/lib/requests');
+// connection mong database
+require('yoom/lib/connectors').connect(function() {
+  // loading models
+  require('yoom/lib/models');
+  // loading controllers
+  require('yoom/lib/controllers');
+  // lifting application
+  app.start();
+});
